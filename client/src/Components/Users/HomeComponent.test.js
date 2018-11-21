3aeab1efccbeab468024247825877a561423d0f7
Your Name <daniel.stine@me.com>
2018-11-21 15:40:33 -0600

    Renamed .jsx files to .js so that Jest tests would run

--- client/src/Components/Users/HomeComponent.test.js
+++ client/src/Components/Users/HomeComponent.test.js
@@ -2,3 +2,10 @@ import React from 'react';
 import {HomeComponent} from './HomeComponent.jsx';
 import renderer from 'react-test-renderer';
 
+test('renders without crashing', () => {
+    const div = document.createElement("div");
+    const render = renderer.create(
+        <HomeComponent />, div
+    );
+    render.unmount(div);
+});
