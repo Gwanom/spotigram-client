569de314ff99c191bfdbb2ccb9ac53ceeeff3fb0
Daniel Stine <daniel.stine@me.com>
2018-11-27 17:50:46 -0600

    Added UI views to edit and view profile components

--- client/src/Components/Users/ViewProfileComponent.js
+++ client/src/Components/Users/ViewProfileComponent.js
@@ -1,10 +1,20 @@
 import React from "react";
 
 export class ViewProfileComponent extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    
     render() {
         return (
             <React.Fragment>
-
+                <div className="viewProfile">
+                    Username: {this.props.user.username}<br />
+                    Name: {this.props.user.firstName} {this.props.user.lastName}<br />
+                    Email: {this.props.user.email}<br />
+                    Banned: {this.props.user.isbanned}<br />
+                    Last Logged In: {this.props.user.lastLogin}
+                </div>
             </React.Fragment>
         )
     }
