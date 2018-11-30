fa424c9981a73a0468622cedbd5a91d636fd7ae8
Your Name <daniel.stine@me.com>
2018-11-30 09:17:07 -0600

    There we go, the other thread components are finally in the directory

---
+++ client/src/Components/Thread/PostsComponent.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+import { connect } from "react-redux";
+import * as userAction from "../../Redux/Actions/User.Actions";
+import ProfileComponent  from "./ProfileComponent";
+import {PostComponent} from "./PostComponent";
+import PropTypes from 'prop-types';
+
+export class PostsComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            type: "",
+            value: "",
+            posts: null
+            
+        }
+    }
+    
+    componentDidMount() {
+        switch (this.props.value) {
+            let path = ""
+            case "song":
+                path = `/songs/title/${this.state.value}`;
+                break;
+            case "album":
+                path = `/albums/title/${this.state.value}`;
+                break;
+            case "artist":
+                path = `/artists/name/${this.state.value}`;
+                break;
+            case "user":
+                path = `/posts/author/${this.state.value}`;
+                break;
+        }
+        fetch(`http://spotigram-env-1.m2phmu28yw.us-east-2.elasticbeanstalk.com${path}`)
+        .then(resp => resp.json())
+        .then(data => {
+            posts: data
+        })
+    }
+    
+    render() {
+        return (
+            <React.Fragment>
+                {this.state.replies.map(replies => 
+                    <PostComponent post={this.state.posts}>)}
+            </React.Fragment>
+        )
+    }
+}
+
+ReplyComponent.propTypes = {
+    type: PropTypes.shape({
+        type: PropTypes.string.isRequired;
+        value: PropTypes.string.isRequired;
+    })
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(PostComponent);PostComponent.propTypes = {
+    post: PropTypes.shape({
+        id: PropTypes.number.isRequired;
+    })
+}
+
+const mapStateToProps = (state) =>{
+    return{
+        userData: state.userReducer
+    }
+}
+
+const mapDispatchToProps = {
+    storeData: userAction.storeData
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(PostComponent);
---
+++ client/src/Components/Thread/ReplyComponent.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { connect } from "react-redux";
+import * as userAction from "../../Redux/Actions/User.Actions";
+import ProfileComponent  from "./ProfileComponent";
+import PropTypes from 'prop-types';
+
+export class ReplyComponent extends React.Component{
+
+    render(){
+        return(
+            <React.Fragment>
+                {this.props.reply.author} says "{this.props.reply.content}"
+            </React.Fragment>           
+        )
+    }
+}
+
+ReplyComponent.propTypes = {
+    reply: PropTypes.shape({
+        author: PropTypes.string.isRequired;
+        content: PropTypes.string.isRequired;
+    })
+}
+
+const mapStateToProps = (state) =>{
+    return{
+        userData: state.userReducer
+    }
+}
+
+const mapDispatchToProps = {
+    storeData: userAction.storeData
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(PostComponent);
