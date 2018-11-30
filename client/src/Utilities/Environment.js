a6ef051c0d79a43ba897b6a4c925d442401e45d3
Jose <Joe.Antonio.Perez@gmail.com>
2018-11-30 15:41:58 -0600

    derp

--- client/src/Utilities/Environment.js
+++ client/src/Utilities/Environment.js
@@ -2,5 +2,5 @@
     spotigramURL: "http://spotigram-env-1.m2phmu28yw.us-east-2.elasticbeanstalk.com/"
   };
   
-  export const environment = process.env.NODE_ENV === "production" ? prod : dev;
+  export const environment = prod 
   
