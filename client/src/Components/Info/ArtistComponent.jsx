e9da9e5f3c985aa30aa2daa4cbf87ebc00994e4f
Your Name <daniel.stine@me.com>
2018-12-01 11:52:45 -0600

    Added preliminary work on Albums, Artists, and Songs, added PropTypes back to Posts

--- client/src/Components/Info/ArtistComponent.jsx
+++ client/src/Components/Info/ArtistComponent.jsx
@@ -1,13 +1,36 @@
 import React from "react";
 import './profile.css';
+import * as userAction from "../../Redux/Actions/User.Actions";
+import { connect } from "react-redux";
+import SpotigramClient from "../../Utilities/HTTPHelper";
+import PostsComponent from "../Thread/PostsComponent"
 
 export class ArtistComponent extends React.Component{
 
     render(){
         return(
             <React.Fragment>
-                
+                {this.props.artist.name} is an artist.
+                {this.props.artist.name} began performing in {this.props.artist.startDate} and ended performing in {this.props.artist.endDate}.<br />
+                Here are some posts about {this.props.artist.name}:<br />
+                <PostsComponent type="artist" value={this.props.artist.name}/>
             </React.Fragment>           
         )
     }
 }
+
+ArtistComponent.propTypes = {
+    artist: PropTypes.object.isRequired
+}
+
+const mapStateToProps = (state) =>{
+    return{
+        userData: state.userReducer
+    }
+}
+
+const mapDispatchToProps = {
+    storeData: userAction.storeData
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(ArtistComponent);
--- client/src/Components/Info/SongComponent.jsx
+++ client/src/Components/Info/SongComponent.jsx
@@ -1,13 +1,40 @@
 import React from "react";
 import './profile.css';
+import * as userAction from "../../Redux/Actions/User.Actions";
+import { connect } from "react-redux";
+import SpotigramClient from "../../Utilities/HTTPHelper";
+import PostsComponent from "../Thread/PostsComponent"
 
 export class SongComponent extends React.Component{
 
     render(){
         return(
             <React.Fragment>
-                
+                <div className="song-info">
+                    {this.props.title} is a song by {this.props.artist}<br />
+                    {this.props.title} is on the album {this.props.album}<br />
+                    Here are some posts about it:<br />
+                    <PostsComponent type="song" value={this.props.title} />
+                </div>
             </React.Fragment>           
         )
     }
 }
+
+SongComponent.propTypes = {
+    title: PropTypes.string.isRequired,
+    album: PropTypes.string.isRequired,
+    artist: PropTypes.string.isRequired
+}
+
+const mapStateToProps = (state) =>{
+    return{
+        userData: state.userReducer
+    }
+}
+
+const mapDispatchToProps = {
+    storeData: userAction.storeData
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(SongComponent);
