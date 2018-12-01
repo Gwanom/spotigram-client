584feb37319788a0f3a7bf4bda1428e207ad4f0c
Jose <Joe.Antonio.Perez@gmail.com>
2018-12-01 10:31:50 -0600

    a

--- client/src/Components/Users/ProfileComponent.jsx
+++ client/src/Components/Users/ProfileComponent.jsx
@@ -4,47 +4,17 @@ import NavbarComponent  from '../NavbarComponent';
 import * as userAction from "../../Redux/Actions/User.Actions";
 import { connect } from "react-redux";
 import SpotigramClient from "../../Utilities/HTTPHelper";
-import  ProfileRender  from '../Renders/ProfileRender';
+import PostsComponent from "../Thread/PostsComponent"
 
 export class ProfileComponent extends React.PureComponent {
-    constructor(props){
-        super(props);
-        this.state={
-            visible:false
-        }
-    }
 
-
-
-    componentDidMount(){
-        console.log(this.props.userData);
-        console.log(this.props.userData.userEntries.username);
-        SpotigramClient.get(`posts/author/${this.props.userData.userEntries.username}`).then(
-            resp=>{
-                this.props.storeThreads(resp.data);
-                this.setState({
-                    ...this.state,
-                    visible:true
-                })
-            })
-        
-    }
     
     render() {
-        console.log(this.props.userData.threadEntries);
         return (
             <React.Fragment>
-                {/* <NavbarComponent user_data={this.props.userData.userEntries.username}/> */}
-                <p>Replies</p>
-                <div className="Threads">
-                    {
-                        this.state.visible === true &&
-                         this.props.userData.threadEntries.map(replies =>
-                            // console.log(replies.postId)
-                                <ProfileRender history={this.props.history} key={replies.postId} username={this.props.userData.userEntries.username} identifier={replies.postId} reply={replies}/>
-
-                            )
-                    }
+                {/* <NavbarComponent user_data={this.props.userData.userEntries.info[0]}/> */}
+                <div className="">
+                    <PostsComponent type="user" value={this.props.userData.userEntries.username} />
                 </div>
             </React.Fragment>
         )
@@ -52,14 +22,12 @@ export class ProfileComponent extends React.PureComponent {
 }
 const mapStateToProps = (state) =>{
     return{
-        userData: state.userReducer,
-        userThreads: state.userReducer.storeThreads
+        userData: state.userReducer
     }
 }
 
 const mapDispatchToProps = {
-    storeData: userAction.storeData,
-    storeThreads: userAction.storeThreads
+    storeData: userAction.storeData
 }
 
 
